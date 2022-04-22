3cde4f29976155b32ce5e620c8dda9761d7d711f
n3oney <neoney@pm.me>
2022-04-22 19:52:22 +0200

    feat: update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,21 @@ module.exports = {
     './src/**/*.ts'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          900: '#171717'
+        },
+        hotPink: '#ff5adc',
+        blueish: '#3943b7',
+        hotRed: '#cc2936',
+        greenish: '#1b998b'
+      },
+      fontFamily: {
+        header: ['Montserrat', 'sans-serif'],
+        body: ['Roboto', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
