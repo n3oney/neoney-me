63f408cf6e908e20265452adc843657b5c672534
n3oney <neoney@pm.me>
2022-04-22 21:27:23 +0200

    feat: add new tailwind variant

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,32 @@
 module.exports = {
-  content: [
-    // Everything is separated because bracket expansion doesn't want to work
-    './src/**/*.svelte',
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.ts'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: {
-          900: '#171717'
-        },
-        hotPink: '#ff5adc',
-        blueish: '#3943b7',
-        hotRed: '#cc2936',
-        greenish: '#1b998b'
-      },
-      fontFamily: {
-        header: ['Montserrat', 'sans-serif'],
-        body: ['Roboto', 'sans-serif']
-      }
-    },
-  },
-  plugins: [],
-}
+	content: [
+		// Everything is separated because bracket expansion doesn't want to work
+		'./src/**/*.svelte',
+		'./src/**/*.html',
+		'./src/**/*.js',
+		'./src/**/*.ts'
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: {
+					900: '#171717'
+				},
+				hotPink: '#ff5adc',
+				blueish: '#3943b7',
+				hotRed: '#cc2936',
+				greenish: '#1b998b'
+			},
+			fontFamily: {
+				header: ['Montserrat', 'sans-serif'],
+				body: ['Roboto', 'sans-serif']
+			}
+		}
+	},
+	plugins: [
+		function ({ addVariant }) {
+			addVariant('child', '& > *');
+			addVariant('child-hover', '& > *:hover');
+		}
+	]
+};
