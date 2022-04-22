6118d210f6c8c528dbaa4152cd43fd6ddb4043f0
n3oney <neoney@pm.me>
2022-04-22 18:46:19 +0200

    feat: add tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-2xl font-semibold">Welcome to SvelteKit</h1>
+<p class="text-lg">
+	Visit <a
+		class="text-blue-600 font-semibold decoration-blue-500 underline decoration-2 underline-offset-1"
+		href="https://kit.svelte.dev">kit.svelte.dev</a
+	> to read the documentation
+</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    // Everything is separated because bracket expansion doesn't want to work
+    './src/**/*.svelte',
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.ts'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
