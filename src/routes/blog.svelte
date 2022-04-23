360241013af42af7d30a7889797251cb50ee7863
n3oney <neoney@pm.me>
2022-04-23 15:41:18 +0200

    fix: fix blog text alignment

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,3 +1,3 @@
-<section id='blog' class='flex justify-center items-center h-screen'>
+<section id='blog' class='p-16 flex justify-center items-center h-screen text-center'>
     <h1 class='font-header font-extrabold text-4xl'>Oops, sorry! This page isn't yet completed.</h1>
 </section>
