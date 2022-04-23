1df13e632a49d8ff4577282ad89762f2d838a26d
n3oney <neoney@pm.me>
2022-04-23 08:03:56 +0200

    style: format code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,9 @@
 <script>
 	import HomeSection from '../components/HomeSection.svelte';
-    import SkillsSection from '../components/SkillsSection.svelte';
+	import SkillsSection from '../components/SkillsSection.svelte';
 	import ProjectsSection from '../components/ProjectsSection.svelte';
 </script>
 
 <HomeSection />
 <SkillsSection />
 <ProjectsSection />
-
