2df97e068ad4d6e8706c0d0df94493b687cace9a
n3oney <neoney@pm.me>
2022-04-23 07:55:09 +0200

    feat: create Project component

---
+++ src/components/Project.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let title: string;
+	export let url: string;
+</script>
+
+<a href={url}>
+	<div class="bg-background-800 rounded-lg p-4">
+		<h2 class="text-hotPink font-header font-bold text-2xl">{title}</h2>
+		<div class="font-medium mt-2 text-lg">
+			<slot />
+		</div>
+	</div>
+</a>
