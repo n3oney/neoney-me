7b624f62d6e4fc848c45fc35e69b8a9d45bef801
n3oney <neoney@pm.me>
2022-04-23 07:59:58 +0200

    refactor: delete additional div

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -3,11 +3,9 @@
 	export let url: string;
 </script>
 
-<a href={url}>
-	<div class="bg-background-800 rounded-lg p-4">
-		<h2 class="text-hotPink font-header font-bold text-2xl">{title}</h2>
-		<div class="font-medium mt-2 text-lg">
-			<slot />
-		</div>
+<a href={url} class="bg-background-800 rounded-lg p-4 flex flex-col justify-center">
+	<h2 class="text-hotPink font-header font-bold text-2xl">{title}</h2>
+	<div class="font-medium mt-2 text-lg">
+		<slot />
 	</div>
 </a>
