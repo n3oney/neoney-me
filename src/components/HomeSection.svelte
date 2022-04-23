38946a7747b2898ba2bbe0776a65858e26e68b24
n3oney <neoney@pm.me>
2022-04-23 07:28:31 +0200

    feat:create HomeSection component

---
+++ src/components/HomeSection.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+    import ScrollArrow from "./ScrollArrow.svelte";
+</script>
+
+<section id="home" class="px-4 flex flex-col justify-center items-center min-h-screen text-center">
+	<div class="h-20 flex-grow" />
+
+	<h1 class="font-header text-6xl font-extrabold">
+		Hi, I'm <span class="text-gradient-animated">neoney</span>
+	</h1>
+	<h2 class="font-header text-lg font-bold mt-1">and I'm a full-stack web developer!</h2>
+
+	<a
+		href="/blog"
+		class="mt-4 bg-gradient-to-r from-hotPink to-blueish rounded-full hover:scale-125 transition-transform"
+	>
+		<div class="bg-background-900 m-1 text-lg px-8 py-1 rounded-full font-header font-bold">
+			<span class="text-gradient">read my blog</span>
+		</div>
+	</a>
+
+	<ScrollArrow href="#skills" />
+</section>
