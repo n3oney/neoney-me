dab667dcf3610c13e4341583276df078126bd1b3
n3oney <neoney@pm.me>
2022-04-23 09:22:47 +0200

    feat: create SocialSection

---
+++ src/components/SocialSection.svelte
@@ -0,0 +1,42 @@
+<script>
+	import IoLogoTwitter from 'svelte-icons/io/IoLogoTwitter.svelte';
+	import FaDiscord from 'svelte-icons/fa/FaDiscord.svelte';
+	import FaMailBulk from 'svelte-icons/fa/FaMailBulk.svelte';
+</script>
+
+<section
+	id="social"
+	class="flex flex-col justify-center items-center min-h-screen border-t border-gray-600/25 font-medium"
+>
+	<h1 class="font-header font-extrabold text-6xl text-center">Contact me!</h1>
+	<table class="text-xl">
+		<tbody>
+			<tr
+				class="cursor-pointer"
+				on:click={() => (window.location.href = 'https://twitter.com/neoneydev')}
+			>
+				<td class="h-[1.5em] text-[#1DA1F2]"><IoLogoTwitter /></td>
+				<td>@neoneydev</td>
+			</tr>
+			<tr>
+				<td class="h-[1.5em] text-[#5865F2]"><FaDiscord /></td>
+				<td>neoney<span class="text-gray-500">#7991</span></td>
+			</tr>
+			<tr class="cursor-pointer" on:click={() => (window.location.href = 'mailto:neoney@pm.me')}>
+				<td class="h-[1.5em]"><FaMailBulk /></td>
+				<td>neoney@pm.me</td>
+			</tr>
+		</tbody>
+	</table>
+</section>
+
+<style>
+	table td:first-child {
+		text-align: right;
+	}
+
+	table {
+		border-spacing: 0.2em 0.5em;
+		border-collapse: separate;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,10 @@
 	import HomeSection from '../components/HomeSection.svelte';
 	import SkillsSection from '../components/SkillsSection.svelte';
 	import ProjectsSection from '../components/ProjectsSection.svelte';
+	import SocialSection from '../components/SocialSection.svelte';
 </script>
 
 <HomeSection />
 <SkillsSection />
 <ProjectsSection />
+<SocialSection />
