15ee68d9d0bbb956c531b9a1b7519aaafeae803d
n3oney <neoney@pm.me>
2022-06-27 17:33:22 +0200

    refactor: update e-mail address

--- src/components/SocialSection.svelte
+++ src/components/SocialSection.svelte
@@ -21,7 +21,7 @@
 		<a class="cursor-pointer justify-self-end h-[1.5em]" href="mailto:neoney@pm.me">
 			<FaMailBulk />
 		</a>
-		<a href="mailto:neoney@pm.me">neoney@pm.me</a>
+		<a href="mailto:neo@neoney.dev">neo@neoney.dev</a>
 	</div>
 </section>
 
